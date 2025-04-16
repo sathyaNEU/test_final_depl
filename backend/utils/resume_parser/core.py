2568f69d2399f37e4f891c6e1fde693273bc4108
Tanmay6461 <pawar.ta@northeastern.edu>
2025-04-15 20:02:04 -0400

    added validations for pdf content

--- backend/utils/resume_parser/core.py
+++ backend/utils/resume_parser/core.py
@@ -13,6 +13,7 @@ from utils.schema import *
 import os
 from utils.snowflake.snowflake_connector import *
 import logging
+import re
 
 load_dotenv()
 
@@ -62,7 +63,26 @@ def extract_text_from_pdf(content):
         return {"text" : text ,
                 "links": hyperlinks}
 
+def is_resume(text):
 
+    resume_sections = [
+        "work experience", "education", "skills", "certifications",
+        "professional summary", "objective", "projects", "publications",
+        "awards", "volunteer experience", "references", "contact details"
+    ]
+
+    lower_txt = text
+
+    section_count = sum(1 for section in resume_sections if section in lower_txt or section.upper() in text )
+
+    has_email = bool(re.search(r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}', text))
+    has_phone = bool(re.search(r'(\+\d{1,3}[-.\s]?)?(\(?\d{3}\)?[-.\s]?)?\d{3}[-.\s]?\d{4}', text))
+
+
+    if(section_count >=3) and (has_email or has_phone):
+        return True
+    else:
+        return False
 
 
 def get_structured_data(user_email, changes, mode='generate'):
@@ -72,38 +92,46 @@ def get_structured_data(user_email, changes, mode='generate'):
     # Configure the Gemini API
     logging.info('calling get structured data - ', changes, mode)
     genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
     try:
-        # Extract text
-        if mode=='generate':
             pdf_url = get_this_column(user_email, 'resume_pdf_url')
             response = requests.get(pdf_url)
-            data = extract_text_from_pdf(response.content) 
-        else:
-            data = get_this_column(user_email, 'resume_json')
-        logging.info(data)
-        # Create model and chat
-        model = genai.GenerativeModel(
-        model_name="gemini-1.5-pro",
-        generation_config=generation_config,
-        system_instruction=sys_resume_generate_prompt if mode == 'generate' else sys_resume_revise_prompt
-        ) 
-     
-        chat = model.start_chat(history=[])
-        
-        # Send message and get response
+            extracted_text = extract_text_from_pdf(response.content) 
+            text=extracted_text['text'] 
+            valid_resume = is_resume(text)
 
-        response = chat.send_message(user_resume_generate_prompt.format(data) if mode=='generate' else user_resume_revise_prompt.format(data, changes))
-        response_text = response.text.strip()
+            # Extract text 
+            if valid_resume:
+                logging.info("valid r")
+                if mode=='generate':
+                    data = extracted_text
+                else:
+                    data = get_this_column(user_email, 'resume_json')
+                # Create model and chat
+                model = genai.GenerativeModel(
+                model_name="gemini-1.5-pro",
+                generation_config=generation_config,
+                system_instruction=sys_resume_generate_prompt if mode == 'generate' else sys_resume_revise_prompt
+                ) 
+            
+                chat = model.start_chat(history=[])
+                
+                # Send message and get response
+                response = chat.send_message(user_resume_generate_prompt.format(data) if mode=='generate' else user_resume_revise_prompt.format(data, changes))
+                response_text = response.text.strip()
 
-        # Handle formatting
-        if response_text.startswith("```json"):
-            response_text = response_text.split("```json")[1]
-        if "```" in response_text:
-            response_text = response_text.split("```")[0]
-        
-        # Parse JSON
-        cleaned_content = json.loads(response_text.strip())
-        return cleaned_content
-        
+                # Handle formatting
+                if response_text.startswith("```json"):
+                    response_text = response_text.split("```json")[1]
+                if "```" in response_text:
+                    response_text = response_text.split("```")[0]
+                
+                # Parse JSON
+                cleaned_content = json.loads(response_text.strip())
+                return cleaned_content
+            else:
+                return "Upload a valid resume"
+            
     except Exception as e:
-        return {"error structuring": str(e)}
+            return {"error structuring": str(e)}
+   
